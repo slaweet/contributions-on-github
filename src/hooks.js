a9d009b5bbbd5d334db56b820b3cbf937c7f3291
Vit Stanislav <slaweet@gmail.com>
2019-10-21 22:13:21 +0200

    ♻️  Move API call from hooks to utils

--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,7 @@
 import { to } from 'await-to-js';
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+
+import { getCommits } from './utils';
 
 // eslint-disable-next-line import/prefer-default-export
 export function useCommits({
@@ -9,7 +10,6 @@ export function useCommits({
   const [commits, setCommits] = useState([]);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-  const githubToken = localStorage.getItem('githubToken');
 
   function transformCommit({ commit, author, sha }) {
     return {
@@ -31,14 +31,9 @@ export function useCommits({
 
   async function fetchUrl(page = 1, prevCommits = []) {
     setLoading(true);
-    const [err, response] = await to(axios.get(
-      `https://api.github.com/repos/${username}/${repo}/commits`, {
-        params: { page, since, until },
-        headers: {
-          ...(githubToken && { Authorization: `Bearer ${githubToken}` }),
-        },
-      },
-    ));
+    const [err, response] = await to(getCommits({
+      repo, username, page, since, until,
+    }));
     setLoading(false);
     if (err) {
       setError(err);
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import moment from 'moment';
 
 export const getRepoUrl = ({ username, repo }) => (
@@ -10,3 +11,16 @@ export const getAvatarSrc = (id) => (
 export const formatDate = (date) => moment(date).format('YYYY-MM-DD');
 
 export const formatDateAndTime = (date) => moment(date).format('YYYY-MM-DD HH:mm');
+
+const githubToken = localStorage.getItem('githubToken');
+
+export const getCommits = ({
+  username, repo, page, since, until,
+}) => axios.get(
+  `https://api.github.com/repos/${username}/${repo}/commits`, {
+    params: { page, since, until },
+    headers: {
+      ...(githubToken && { Authorization: `Bearer ${githubToken}` }),
+    },
+  },
+);
