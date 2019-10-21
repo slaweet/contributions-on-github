16c59a38e8b77e0314d7cc22079dd5ff277f95ce
Vit Stanislav <slaweet@gmail.com>
2019-10-21 06:50:42 +0200

    🌱 Load config from url search params

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 
 import { Col, Container, Row } from 'reactstrap';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
 
 import CommitsPage from './CommitsPage';
@@ -9,13 +10,16 @@ import CommitsPage from './CommitsPage';
 function App() {
   return (
     <Router>
-      <Container>
-        <Row>
-          <Col>
-            <CommitsPage />
-          </Col>
-        </Row>
-      </Container>
+      <QueryParamProvider ReactRouterRoute={Route}>
+        <Container>
+          <Row>
+            <Col>
+              <CommitsPage />
+            </Col>
+          </Row>
+        </Container>
+      </QueryParamProvider>
+
     </Router>
   );
 }
--- src/CommitsPage.js
+++ src/CommitsPage.js
@@ -1,7 +1,7 @@
 import {
   Card, CardHeader, ListGroup, ListGroupItem,
 } from 'reactstrap';
-import { useParams } from 'react-router-dom';
+import { useQueryParam, StringParam, BooleanParam } from 'use-query-params';
 import Emoji from 'react-emoji-render';
 import React from 'react';
 
@@ -11,27 +11,26 @@ import PunchChart from './PunchChart';
 
 
 export default function CommitsPage() {
-  const {
-    username = 'LiskHQ',
-    repo = 'lisk-hub',
-    since = '2019-09-29T00:00:00+0000',
-    until = '2019-10-11T00:00:00+0000',
-    showAvatarsAsPoints = false,
-  } = useParams();
+  const [repo = 'lisk-hub'] = useQueryParam('repo', StringParam);
+  const [username = 'LiskHQ'] = useQueryParam('username', StringParam);
+  const [since = '2019-09-29T00:00:00+0000'] = useQueryParam('since', StringParam);
+  const [until = '2019-10-11T00:00:00+0000'] = useQueryParam('until', StringParam);
+  const [showAvatarsAsPoints = false] = useQueryParam('showAvatarsAsPoints', BooleanParam);
+
   const config = {
     username, repo, since, until, showAvatarsAsPoints,
   };
   const [commits] = useCommits(config);
   return (
     <Card>
-      <CardHeader>{`Commits of ${config.repo} from ${formatDate(config.since)} to ${formatDate(config.until)}`}</CardHeader>
+      <CardHeader>{`Commits of ${username}/${repo} from ${formatDate(since)} to ${formatDate(until)}`}</CardHeader>
       <PunchChart commits={commits} config={config} />
       <ListGroup>
         {commits.map(({
           sha, message, author, date,
         }) => (
           <ListGroupItem key={sha}>
-            <a href={`https://github.com/${config.username}/${config.repo}/commit/${sha}`}>
+            <a href={`https://github.com/${username}/${repo}/commit/${sha}`}>
               {sha.substr(0, 6)}
             </a>
             {' '}
