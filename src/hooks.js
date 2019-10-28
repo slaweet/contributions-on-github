84010b52b47ff947f3d3bd3020c719f963172263
Vit Stanislav <slaweet@gmail.com>
2019-10-28 22:15:44 +0100

    ♻  Simplify useCommits hook

--- src/hooks.js
+++ src/hooks.js
@@ -21,6 +21,22 @@ export function useQueryParamConfig() {
   }];
 }
 
+function transformCommit({
+  // eslint-disable-next-line camelcase
+  commit, author, sha, html_url, committer,
+}) {
+  return {
+    author,
+    date: commit.author.date,
+    message: commit.message,
+    messageHeadline: commit.message.split('\n')[0],
+    sha,
+    comment_count: commit.comment_count,
+    html_url,
+    committer,
+  };
+}
+
 export function useCommits({
   username, repo, since, until,
 }) {
@@ -28,57 +44,38 @@ export function useCommits({
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
-  function transformCommit({
-    // eslint-disable-next-line camelcase
-    commit, author, sha, html_url, committer,
-  }) {
-    return {
-      author,
-      date: commit.author.date,
-      message: commit.message,
-      messageHeadline: commit.message.split('\n')[0],
-      sha,
-      comment_count: commit.comment_count,
-      html_url,
-      committer,
-    };
-  }
-
-  function validateConfig() {
-    const isValid = !!repo && !!username;
-    if (!isValid) {
-      setError('Missing `repo` and/or `username` param. Please select them in the header dropdown');
+  useEffect(() => {
+    function validateConfig() {
+      const isValid = !!repo && !!username;
+      if (!isValid) {
+        setError('Missing `repo` and/or `username` param. Please select them in the header dropdown');
+      }
+      return isValid;
     }
-    return isValid;
-  }
 
-  async function fetchUrl(page = 1, prevCommits = []) {
-    setLoading(true);
-    const [err, response] = await to(getCommits({
-      repo, username, page, since, until,
-    }));
-    setLoading(false);
-    if (err) {
-      setError(err);
-    } else {
-      const currentCommits = [
-        ...prevCommits,
-        ...response.data.map(transformCommit).filter(
-          ({ sha }) => !prevCommits.some((c) => c.sha === sha),
-        )];
-      setCommits(currentCommits);
-      if (response.data.length === 30) {
-        fetchUrl(page + 1, currentCommits);
+    async function fetchUrl(page = 1, prevCommits = []) {
+      setLoading(true);
+      const [err, response] = await to(getCommits({
+        repo, username, page, since, until,
+      }));
+      setLoading(false);
+      if (err) {
+        setError(err);
+      } else {
+        const currentCommits = [
+          ...prevCommits,
+          ...response.data.map(transformCommit).filter(
+            ({ sha }) => !prevCommits.some((c) => c.sha === sha),
+          )];
+        setCommits(currentCommits);
+        if (response.data.length === 30) {
+          fetchUrl(page + 1, currentCommits);
+        }
       }
     }
-  }
-
-  useEffect(() => {
     if (validateConfig()) {
       fetchUrl();
     }
-    return function cleanup() {
-    };
   }, [username, repo, since, until]);
   return [commits, loading, error];
 }
