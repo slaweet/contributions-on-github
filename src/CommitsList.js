c13dcbd60839488e306c6c7f7d4285e8583d1abc
Vit Stanislav <slaweet@gmail.com>
2019-10-28 21:45:50 +0100

    ♻ Separate CommitsList out of CommitsPage

---
+++ src/CommitsList.js
@@ -0,0 +1,47 @@
+import './CommitsList.css';
+
+import { Button, ListGroup, ListGroupItem } from 'reactstrap';
+import Emoji from 'react-emoji-render';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { formatDateAndTime } from './utils';
+
+
+export default function CommitsList({ commits }) {
+  return (
+    <ListGroup>
+      {commits.map(({
+        sha, messageHeadline, author, date, html_url: url, committer,
+      }) => (
+        <ListGroupItem key={sha} className="commitRow">
+          <div>
+            <div>
+              <strong>
+                <Emoji text={messageHeadline} />
+              </strong>
+            </div>
+            <div>
+              <a href={committer.html_url} target="_blank" rel="noopener noreferrer">
+                <img src={committer.avatar_url} alt="avatar" className="avatar" />
+                {` ${author.login}`}
+              </a>
+              {` committed on ${formatDateAndTime(date)} `}
+            </div>
+          </div>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <Button>{sha.substr(0, 6)}</Button>
+          </a>
+        </ListGroupItem>
+      ))}
+    </ListGroup>
+  );
+}
+
+CommitsList.propTypes = {
+  commits: PropTypes.arrayOf(PropTypes.shape()),
+};
+
+CommitsList.defaultProps = {
+  commits: [],
+};
--- src/CommitsPage.js
+++ src/CommitsPage.js
@@ -1,24 +1,13 @@
 import {
-  Alert,
-  Button,
-  Card,
-  CardBody,
-  CardHeader,
-  ListGroup,
-  ListGroupItem,
-  Spinner,
+  Alert, Card, CardBody, CardHeader, Spinner,
 } from 'reactstrap';
 import { useQueryParam, StringParam, BooleanParam } from 'use-query-params';
-import Emoji from 'react-emoji-render';
 import React from 'react';
 import moment from 'moment';
-import './CommitsPage.css';
 
-import {
-  formatDate,
-  formatDateAndTime,
-} from './utils';
+import { formatDate } from './utils';
 import { useCommits } from './hooks';
+import CommitsList from './CommitsList';
 import ConfigFormButton from './ConfigFormButton';
 import PunchChart from './PunchChart';
 
@@ -51,31 +40,7 @@ export default function CommitsPage() {
           ? <Alert color="danger">{`${error}`}</Alert>
           : <PunchChart commits={commits} config={config} />}
       </CardBody>
-      <ListGroup>
-        {commits.map(({
-          sha, messageHeadline, author, date, html_url, committer,
-        }) => (
-          <ListGroupItem key={sha} className="commitRow">
-            <div>
-              <div>
-                <strong>
-                  <Emoji text={messageHeadline} />
-                </strong>
-              </div>
-              <div>
-                <a href={committer.html_url} target="_blank" rel="noopener noreferrer">
-                  <img src={committer.avatar_url} alt="avatar" className="avatar" />
-                  {` ${author.login}`}
-                </a>
-                {` committed on ${formatDateAndTime(date)} `}
-              </div>
-            </div>
-            <a href={html_url} target="_blank" rel="noopener noreferrer">
-              <Button>{sha.substr(0, 6)}</Button>
-            </a>
-          </ListGroupItem>
-        ))}
-      </ListGroup>
+      <CommitsList commits={commits} />
     </Card>
   );
 }
