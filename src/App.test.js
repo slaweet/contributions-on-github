d32a492ad2d459c170742694d5198b6469e63cf8
Vit Stanislav <slaweet@gmail.com>
2019-10-29 06:36:32 +0100

    ✅ Add tests of commits page

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react';
 import React from 'react';
+import axios from 'axios';
+
+import { createMemoryHistory } from 'history';
+import { render, act } from '@testing-library/react';
 
 import App from './App';
+import commitsApiReponse from '../test/mocks/commitsApiReponse';
 
 function renderWithRouter(
   ui,
@@ -15,11 +18,29 @@ function renderWithRouter(
   };
 }
 
+localStorage.setItem('githubToken', 'SECRET TOKEN');
+
 describe('App', () => {
   const route = '/?repo=lisk-hub&username=LiskHQ';
+
+  it('renders message if config not provided', () => {
+    const { container } = renderWithRouter(<App />);
+    expect(container.innerHTML).toContain('Missing `repo` and/or `username` param');
+  });
 
   it('renders empty state with 0 commits at first', () => {
     const { container } = renderWithRouter(<App />, { route });
     expect(container.innerHTML).toContain('0 commits');
+  });
+
+  it('renders commits page with 2 commits from the api', async () => {
+    jest.spyOn(axios, 'get').mockImplementationOnce(() => Promise.resolve(
+      { data: commitsApiReponse },
+    ));
+    let wrapper;
+    await act(async () => {
+      wrapper = renderWithRouter(<App />, { route });
+    });
+    expect(wrapper.container.innerHTML).toContain('2 commits');
   });
 });
