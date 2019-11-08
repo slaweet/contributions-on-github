63ad53531887126f5e31cb93425c0e7c12045390
Vit Stanislav <slaweet@gmail.com>
2019-11-08 07:54:43 +0100

    ♻️  Merge common logic into useEvents hook

--- src/hooks.js
+++ src/hooks.js
@@ -21,10 +21,10 @@ export function useQueryParamConfig() {
   }];
 }
 
-export function useCommits({
+function useEvents({
   username, repo, since, until,
-}) {
-  const [commits, setCommits] = useState([]);
+}, getEvents) {
+  const [commits, setEvents] = useState([]);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
@@ -37,72 +37,37 @@ export function useCommits({
       return isValid;
     }
 
-    async function fetchUrl(page = 1, prevCommits = []) {
+    async function fetchUrl(page = 1, prevEvents = []) {
       setLoading(true);
-      const [err, response] = await to(getCommits({
+      const [err, response] = await to(getEvents({
         repo, username, page, since, until,
       }));
       setLoading(false);
       if (err) {
         setError(err);
       } else {
-        const currentCommits = [
-          ...prevCommits,
+        const currentEvents = [
+          ...prevEvents,
           ...response.data.filter(
-            ({ id }) => !prevCommits.some((c) => c.id === id),
+            ({ id }) => !prevEvents.some((c) => c.id === id),
           )];
-        setCommits(currentCommits);
+        setEvents(currentEvents);
         if (response.data.length === 30) {
-          fetchUrl(page + 1, currentCommits);
+          fetchUrl(page + 1, currentEvents);
         }
       }
     }
     if (validateConfig()) {
       fetchUrl();
     }
-  }, [username, repo, since, until]);
+  }, [username, repo, since, until, getEvents]);
   return [commits, loading, error];
 }
 
-export function usePullRequests({
-  username, repo, since, until,
-}) {
-  const [pullRequests, setPRs] = useState([]);
-  const [error, setError] = useState('');
-  const [loading, setLoading] = useState(false);
+export function useCommits(config) {
+  return useEvents(config, getCommits);
+}
 
-  useEffect(() => {
-    function validateConfig() {
-      const isValid = !!repo && !!username;
-      if (!isValid) {
-        setError('Missing `repo` and/or `username` param. Please select them in the header dropdown');
-      }
-      return isValid;
-    }
-
-    async function fetchUrl(page = 1, prevPullRequests = []) {
-      setLoading(true);
-      const [err, response] = await to(getPullRequests({
-        repo, username, page, since, until,
-      }));
-      setLoading(false);
-      if (err) {
-        setError(err);
-      } else {
-        const currentPullRequests = [
-          ...prevPullRequests,
-          ...response.data.filter(
-            ({ id }) => !prevPullRequests.some((c) => c.id === id),
-          )];
-        setPRs(currentPullRequests);
-        if (response.data.length === 30) {
-          fetchUrl(page + 1, currentPullRequests);
-        }
-      }
-    }
-    if (validateConfig()) {
-      fetchUrl();
-    }
-  }, [username, repo, since, until]);
-  return [pullRequests, loading, error];
+export function usePullRequests(config) {
+  return useEvents(config, getPullRequests);
 }
