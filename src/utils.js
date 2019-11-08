459d26670278fd4c8ad5231479b09db39c7bfc64
Vit Stanislav <slaweet@gmail.com>
2019-11-08 19:20:43 +0100

    ♻️  Create and use getFromGithub helper function

--- src/utils.js
+++ src/utils.js
@@ -7,6 +7,17 @@ export const formatDateAndTime = (date) => moment(date).format('YYYY-MM-DD HH:mm
 
 const githubToken = localStorage.getItem('githubToken');
 
+function getFromGithub(path, { username, repo, ...params }) {
+  return axios.get(
+    `https://api.github.com/repos/${username}/${repo}${path}`, {
+      params,
+      headers: {
+        ...(githubToken && { Authorization: `Bearer ${githubToken}` }),
+      },
+    },
+  );
+}
+
 function transformCommit({
   // eslint-disable-next-line camelcase
   commit, author, sha, html_url,
@@ -25,14 +36,9 @@ function transformCommit({
 export const getCommits = async ({
   username, repo, page, since, until,
 }) => {
-  const response = await axios.get(
-    `https://api.github.com/repos/${username}/${repo}/commits`, {
-      params: { page, since, until },
-      headers: {
-        ...(githubToken && { Authorization: `Bearer ${githubToken}` }),
-      },
-    },
-  );
+  const response = await getFromGithub('/commits', {
+    username, repo, page, since, until,
+  });
   response.data = response.data.map(transformCommit);
   return response;
 };
@@ -55,14 +61,9 @@ function transformPullRequest({
 export const getPullRequests = async ({
   username, repo, page, since, until,
 }) => {
-  const response = await axios.get(
-    `https://api.github.com/repos/${username}/${repo}/pulls`, {
-      params: { page, state: 'closed' },
-      headers: {
-        ...(githubToken && { Authorization: `Bearer ${githubToken}` }),
-      },
-    },
-  );
+  const response = await getFromGithub('/pulls', {
+    username, repo, page, state: 'closed',
+  });
   response.data = response.data.map(transformPullRequest).filter((pr) => (
     (moment(pr.date).isAfter(since))
     && moment(pr.date).isBefore(until)
