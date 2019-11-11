8e2821d6a72871bd44c16ffbe464d16118b8c2b1
Vit Stanislav <slaweet@gmail.com>
2019-11-11 08:11:10 +0100

    ♻️  Avoid duplication in utils.js

--- src/utils.js
+++ src/utils.js
@@ -7,8 +7,8 @@ export const formatDateAndTime = (date) => moment(date).format('YYYY-MM-DD HH:mm
 
 const githubToken = localStorage.getItem('githubToken');
 
-function getFromGithub(path, { username, repo, ...params }) {
-  return axios.get(
+async function getFromGithub(path, { username, repo, ...params }, transformItem) {
+  const response = await axios.get(
     `https://api.github.com/repos/${username}/${repo}${path}`, {
       params,
       headers: {
@@ -16,6 +16,8 @@ function getFromGithub(path, { username, repo, ...params }) {
       },
     },
   );
+  response.data = response.data.map(transformItem);
+  return response;
 }
 
 function transformCommit({
@@ -33,15 +35,9 @@ function transformCommit({
   };
 }
 
-export const getCommits = async ({
-  username, repo, page, since, until,
-}) => {
-  const response = await getFromGithub('/commits', {
-    username, repo, page, since, until,
-  });
-  response.data = response.data.map(transformCommit);
-  return response;
-};
+export const getCommits = (params) => (
+  getFromGithub('/commits', params, transformCommit)
+);
 
 function transformPullRequest({
   // eslint-disable-next-line camelcase
@@ -58,18 +54,11 @@ function transformPullRequest({
   };
 }
 
-export const getPullRequests = async ({
-  username, repo, page, since, until,
-}) => {
-  const response = await getFromGithub('/pulls', {
-    username, repo, page, state: 'closed',
-  });
-  response.data = response.data.map(transformPullRequest).filter((pr) => (
-    (moment(pr.date).isAfter(since))
-    && moment(pr.date).isBefore(until)
-  ));
-  return response;
-};
+export const getPullRequests = (params) => (
+  getFromGithub('/pulls', {
+    ...params, state: 'closed',
+  }, transformPullRequest)
+);
 
 function transformComment({
   // eslint-disable-next-line camelcase
@@ -86,12 +75,6 @@ function transformComment({
   };
 }
 
-export const getComments = async ({
-  username, repo, page, since, until,
-}) => {
-  const response = await getFromGithub('/pulls/comments', {
-    username, repo, page, since, until,
-  });
-  response.data = response.data.map(transformComment);
-  return response;
-};
+export const getComments = (params) => (
+  getFromGithub('/pulls/comments', params, transformComment)
+);
